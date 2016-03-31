b06eab8b6477979be315079c7b47923eee0d3275
Sergei Chapygin <mollwillcall@gmail.com>
2016-03-31 20:44:29 +0400

    qwertyuiop

--- solutions/MyList/List.cpp
+++ solutions/MyList/List.cpp
@@ -32,7 +32,7 @@ void Sset::merge(int i, int j) {
     if (a != b) {
       if (h[a] < h[b]) {
         val[a] = b;
-	  } else {
+      } else {
         val[b] = a;
         if (h[a] == h[b])
           ++h[a];
